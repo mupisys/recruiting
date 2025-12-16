48823c1b371541733f0d1d7fc6466ca3384079dc
Gabriel Cunha <gabrielantcunha@gmail.com>
2025-12-16 20:09:05 -0300

    feat(messages): create Mensagem model and register it in admin

--- casulo/admin.py
+++ casulo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'data_envio', 'lido')
+    list_filter = ('lido', 'data_envio')
+    search_fields = ('nome', 'email', 'mensagem')
+    ordering = ('-data_envio',)
--- casulo/models.py
+++ casulo/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class BaseModel(models.Model):
+  created_at = models.DateTimeField(_("Criado em"), auto_now_add=True)
+  updated_at = models.DateTimeField(_("Atualizado em"), auto_now=True)
+  is_active = models.BooleanField(_("Ativo"), default=True)
+
+  class Meta:
+    abstract = True
+
+class Message(models.Model):
+    nome = models.CharField(_("Nome"), max_length=120)
+    email = models.EmailField(_("Email"))
+    mensagem  = models.TextField(_("Mensagem"))
+    data_envio = models.DateTimeField(_("Enviado em"), auto_now_add=True)
+    lido = models.BooleanField(_("Lido"), default=False)
+    
+
+    class Meta:
+        verbose_name = _("Messagem")
+        verbose_name_plural = _("Messagens")
+        
+        ordering = ["-data_envio"]
+
+    def __str__(self):
+        return f"{self.nome} <{self.email}>"
+
+    def get_absolute_url(self):
+        return reverse("message_detail", kwargs={"pk": self.pk})
